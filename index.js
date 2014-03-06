ff61ec382516479d298b22495b163190290d4d79
Scott Puleo <puleos@gmail.com>
2014-03-06 15:51:20 -0500

    added support for non object types.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ module.exports = function(object,  hashAlgorithm){
   hashAlgorithm.toLowerCase();
 
   validate(object, hashAlgorithm);
+
+  return hash(object, hashAlgorithm);
 }
 
-function validate(object, algo){
+var validate = function(object, algo){
 	var supported = crypto.getHashes();
 	
 	if(typeof object === 'undefined') { 
@@ -20,5 +22,47 @@ function validate(object, algo){
 		throw new Error('Hash Algorithm "' + algo + '"  not supported. ' + 
 			'Supported values: ' + supported.join(', '));
 	}
-}
+};
+
+var hash = function(object, algo){
+	var hashFn = crypto.createHash(algo);
+	var type = typeof value;
+
+	typeHasher(hashFn).dispatch(object);
+
+  return hashFn.digest('hex');
+};
+
+function typeHasher(hashFn){
+	return {
+		dispatch: function(value){	
+			var type = typeof value;
+			var func = this['_' + type];
+			return func(value);
+		},
+		_object: function(object) {
+			var objType = Object.prototype.toString.call(object);
+			//if(objType === '[object Array]') { 
+			return typeHasher._array.call(this, object);
+		},
+		_boolean: function(bool){
+			return hashFn.update(bool.toString());
+		},
+		_string: function(string){
+			return hashFn.update(string);
+		},
+		_function: function(fn){
+			return hashFn.update(fn.toString());
+		},
+		_number: function(number){
+			return hashFn.update(number.toString());
+		},
+		_xml: function(xml){
+			return hashFn.update(xml.toString());
+		},
+		_array: function(){
+
+		}
+	}
+};
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var hash = require('../index');
+var validSha1 = /^[0-9a-f]{40}$/i;
 
 test('throws when no arguments are passed', function (assert) {
   assert.plan(1);
@@ -11,4 +12,13 @@ test('validates algo if provided', function(assert){
   assert.throws(function(){
     hash({foo: 'bar'}, 'baz');
   });
+});
+
+test('handles non-object types', function(assert){
+  assert.plan(4);
+  var func = function(a){ return a+1; };
+  assert.ok(validSha1.test(hash('Shazbot!')), 'hash string');
+  assert.ok(validSha1.test(hash(42)), 'hash number');
+  assert.ok(validSha1.test(hash(true)), 'hash bool');
+  assert.ok(validSha1.test(hash(func)), 'hash function');
 });
