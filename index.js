d0e127e5198bd0c43c9ebad9e4f1d9464b6466f6
Hauke Henningsen <sqrt@entless.org>
2015-08-26 17:34:57 +0200

    Increase to version 0.9.1
    This includes support for hashing URL objects and fixes a bug
    which occurred when trying to hash native functions.

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function hash(object, options){
 /** Check if the given function is a native function */
 function isNativeFunction(f) {
   var exp = /^function\s+\w*\s*\(\s*\)\s*{\s+\[native code\]\s+}$/i;
-  return exp.exec(String(f)) != null;
+  return exp.exec(Function.prototype.toString.call(f)) != null;
 }
 
 function typeHasher(hashFn, options, context){
