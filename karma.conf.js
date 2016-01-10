55dc4ec105a5da49e2b8624977b6e094622d2f8a
Anna Henningsen <sqrt@entless.org>
2016-01-10 01:00:58 +0100

    karma: Add Firefox to default browsers

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
